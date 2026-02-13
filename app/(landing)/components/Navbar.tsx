e9fdeb15c83cdb80350d7593d771f7c0ff7ede62
GovanDBT <govan.dibotelo@gmail.com>
2026-02-13 23:22:38 +0200

    BOT-4 make nav menu responsive

--- app/(landing)/components/Navbar.tsx
+++ app/(landing)/components/Navbar.tsx
@@ -10,6 +10,17 @@ import {
   NavigationMenuLink,
   NavigationMenuList,
 } from "@/components/ui/navigation-menu";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+import { CircleX, Menu } from "lucide-react";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
 
@@ -21,8 +32,11 @@ const Navbar = () => {
   return (
     <nav className="py-4">
       <div className="container flex items-center justify-between">
+        {/* Logo */}
         <span>Logo</span>
-        <NavigationMenu>
+
+        {/* menu items - Desktop view only */}
+        <NavigationMenu className="hidden lg:inline-flex">
           <NavigationMenuList className="flex gap-8">
             {/* Home */}
             <NavigationMenuItem>
@@ -51,7 +65,94 @@ const Navbar = () => {
             </NavigationMenuItem>
           </NavigationMenuList>
         </NavigationMenu>
-        <Button onClick={() => router.push("/auth/signIn")}>Sign In</Button>
+
+        {/* menu button - Desktop view only */}
+        <Button
+          className="hidden lg:inline-flex"
+          onClick={() => router.push("/auth/signIn")}
+        >
+          Sign In
+        </Button>
+
+        {/* burger / sheet - tablet or mobile view only */}
+        <Sheet>
+          <SheetTrigger asChild className="lg:hidden">
+            <Menu
+              size={28}
+              strokeWidth={2.5}
+              className="text-primary cursor-pointer"
+            />
+          </SheetTrigger>
+          <SheetContent
+            side="left"
+            className="[&>button:first-of-type]:hidden flex flex-col lg:hidden"
+          >
+            {/* Menu Header */}
+            <SheetHeader>
+              <SheetTitle className="flex justify-between items-center">
+                logo
+                <SheetClose
+                  className="cursor-pointer focus:outline-none"
+                  title="close menu"
+                >
+                  <CircleX size={24} className="text-primary" />
+                </SheetClose>
+              </SheetTitle>
+            </SheetHeader>
+
+            {/* Menu items */}
+            <ScrollArea className="flex-1 overflow-y-auto">
+              <NavigationMenu viewport={false} className="max-w-full">
+                <div className="w-full text-center px-4">
+                  <NavigationMenuList className="flex flex-col space-y-4">
+                    {/* Home */}
+                    <NavigationMenuItem className="w-full">
+                      <NavigationMenuLink asChild>
+                        <Link
+                          href="/"
+                          className={`${
+                            pathname === "/" && "active"
+                          } text-[16px]`}
+                        >
+                          Home
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    {/* About Us */}
+                    <NavigationMenuItem className="w-full">
+                      <NavigationMenuLink asChild>
+                        <Link
+                          href="/about"
+                          className={`${
+                            pathname === "/about" && "active"
+                          } text-[16px]`}
+                        >
+                          About Us
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                    {/* Features */}
+                    <NavigationMenuItem className="w-full">
+                      <NavigationMenuLink asChild>
+                        <Link href="/features" className="text-[16px]">
+                          Features
+                        </Link>
+                      </NavigationMenuLink>
+                    </NavigationMenuItem>
+                  </NavigationMenuList>
+                </div>
+              </NavigationMenu>
+            </ScrollArea>
+
+            {/* Menu Buttons */}
+            <SheetFooter>
+              {/* Sign in Button */}
+              <Button onClick={() => router.push("/auth/signIn")}>
+                Sign In
+              </Button>
+            </SheetFooter>
+          </SheetContent>
+        </Sheet>
       </div>
     </nav>
   );
